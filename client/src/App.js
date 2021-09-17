785306f424409ce541f65504079f4c7822a8b5bc
mattmoyka <mmoyka17@gmail.com>
2021-09-17 09:42:18 -0400

    testing deploy update again

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-
+const KEY = process.env.REACT_APP_AIRTABLE_KEY;
+const BASE = process.env.REACT_APP_AIRTABLE_BASE;
 
 function App() {
 
   return (
     <div className="App">
-
+      {KEY} : {BASE}
     </div>
   );
 }
