ccf8744fa86cd2969950facbf1b4133f757f458f
mattmoyka <mmoyka17@gmail.com>
2021-09-20 10:10:45 -0400

    Monday morning css tweaks

--- client/src/Forms/NewroundForm.jsx
+++ client/src/Forms/NewroundForm.jsx
@@ -1,7 +1,7 @@
 export default function NewroundForm(props) {
   return (
     <form onSubmit={props.handleSubmit} className="flex flex-col flex-nowrap items-center text-lg bg-black">
-      <div className='flex items-center bg-green-400  rounded-t-lg border-green-500 mt-10 w-50'>
+      <div className='flex items-center bg-green-600  rounded-t-lg border-green-500 mt-10 w-50'>
         <label className=' w-20 text-right p-4 mr-8 text-green-200'>Course Name:</label>
         <input
           className="flex-1 p-4 pl-0 bg-transparent placeholder-green-300 outline-none text-white overflow-ellipsis overflow-hidden"
@@ -9,7 +9,7 @@ export default function NewroundForm(props) {
           value={props.courseName}
           onChange={(e) => props.setCourseName(e.target.value)} />
       </div>
-      <div className='flex items-center bg-green-400 border-green-500 m-0'>
+      <div className='flex items-center bg-green-600 border-green-500 m-0'>
         <label className='w-20 text-right p-4 mr-8 text-green-200'>Course Par:</label>
         <input
           className="flex-1 p-4 pl-0 bg-transparent placeholder-green-300 outline-none text-white overflow-ellipsis overflow-hidden"
@@ -17,7 +17,7 @@ export default function NewroundForm(props) {
           value={props.coursePar}
           onChange={(e) => props.setCoursePar(e.target.value)} />
       </div>
-      <div className='flex items-center bg-green-400 border-green-500 m-0'>
+      <div className='flex items-center bg-green-600 border-green-500 m-0'>
         <label className='w-20 text-right p-4 mr-8 text-green-200'>Handicap:</label>
         <input
           className="flex-1 p-4 pl-0 bg-transparent placeholder-green-300 outline-none text-white overflow-ellipsis overflow-hidden"
@@ -25,7 +25,7 @@ export default function NewroundForm(props) {
           value={props.handicap}
           onChange={(e) => props.setHandicap(e.target.value)} />
       </div>
-      <div className='flex items-center bg-green-400  border-green-500 m-0'>
+      <div className='flex items-center bg-green-600  border-green-500 m-0'>
         <label className='w-20 text-right p-4 mr-8 text-green-200'>Player Name:</label>
         <input
           className="flex-1 p-4 pl-0 bg-transparent placeholder-green-300 outline-none text-white overflow-ellipsis overflow-hidden"
@@ -33,7 +33,7 @@ export default function NewroundForm(props) {
           value={props.playerName}
           onChange={(e) => props.setPlayerName(e.target.value)} />
       </div>
-      <div className="flex items-center bg-green-400  border-green-500 m-0">
+      <div className="flex items-center bg-green-600  border-green-500 m-0">
         <label className="w-20 text-right p-4 mr-8 text-green-200">Other Players:</label>
         <input
           className="flex-1 p-4 pl-0 bg-transparent placeholder-green-300 outline-none text-white overflow-ellipsis overflow-hidden"
@@ -41,7 +41,7 @@ export default function NewroundForm(props) {
           value={props.otherPlayers}
           onChange={(e) => props.setOtherPlayers(e.target.value)} />
       </div>
-      <div className="flex items-center bg-green-400  rounded-b-lg border-green-500 mb-10">
+      <div className="flex items-center bg-green-600  rounded-b-lg border-green-500 mb-10">
         <label className="w-20 text-right p-4 mr-8 text-green-200">Date:</label>
         <input
           className="flex-1 p-4 pl-0 bg-transparent placeholder-green-300 outline-none text-white overflow-ellipsis overflow-hidden"
@@ -49,7 +49,7 @@ export default function NewroundForm(props) {
           value={props.date}
           onChange={(e) => props.setDate(e.target.value)} />
       </div>
-      <button>Start Round</button>
+      <button className='text-white w-52 h-12 bg-green-600 rounded-lg'>Start Round</button>
     </form >
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <div className='flex flex-row justify-between px-12 bg-green-600'>
       <div className='flex items-center'>
-        <Link to='/'>18 Bogeys</Link>
+        <Link to='/' className='text-xl'>18 Bogeys</Link>
       </div>
-      <nav className='flex flex-col text-justify text-xl'>
+      <nav className='flex flex-col text-justify text-base'>
         <Link to='/'>Home</Link>
         <Link to='/newround'>Start a Round</Link>
         <Link to='/gamefeed'>Game Data</Link>
