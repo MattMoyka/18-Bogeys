dea815cb32d184d9c7b63c1fffd347c5baf2125d
mattmoyka <mmoyka17@gmail.com>
2021-09-21 09:37:23 -0400

    ten through eighteenth hole added to scorecard

--- client/src/Forms/ScorecardForm.jsx
+++ client/src/Forms/ScorecardForm.jsx
@@ -3,8 +3,6 @@
 
 export default function ScorecardForm(props) {
 
-
-
   return (
     <form onSubmit={props.handleSubmit}>
       <div className="grid grid-cols-9 grid-rows-2 text-base justify-items-start m-10 p-5 border-4 border-black bg-green-200 rounded-xl">
@@ -20,6 +18,16 @@ export default function ScorecardForm(props) {
         <input className='w-7 border-4 border-black' type='text' value={props?.seven} onChange={e => props.setSeven(e.target.value)} />
         <input className='w-7 border-4 border-black' type='text' value={props?.eight} onChange={e => props.setEight(e.target.value)} />
         <input className='w-7 border-4 border-black' type='text' value={props?.nine} onChange={e => props.setNine(e.target.value)} />
+        <div>10</div><div>11</div><div>12</div><div>13</div><div>14</div><div>15</div><div>16</div><div>17</div><div>18</div>
+        <input className='w-7 border-4 border-black' type='text' value={props?.ten} onChange={e => props.setTen(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.eleven} onChange={e => props.setEleven(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.twelve} onChange={e => props.setTwelve(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.thirteen} onChange={e => props.setThirteen(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.fourteen} onChange={e => props.setFourteen(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.fifteen} onChange={e => props.setFifteen(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.sixteen} onChange={e => props.setSixteen(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.seventeen} onChange={e => props.setSeventeen(e.target.value)} />
+        <input className='w-7 border-4 border-black' type='text' value={props?.eighteen} onChange={e => props.setEighteen(e.target.value)} />
       </div>
       <button>save score card</button>
     </form>
