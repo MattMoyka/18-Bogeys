2b515a53269f254d90efe6082e6d579845e55245
mattmoyka <mmoyka17@gmail.com>
2021-09-23 15:49:28 -0400

    added gaurd operators

--- client/src/components/Scorecard.jsx
+++ client/src/components/Scorecard.jsx
@@ -50,25 +50,25 @@ export default function Scorecard() {
   useEffect(() => {
     const fetchGame = async () => {
       const res = await axios.get(`${URL}/${id}`, config);
-      const { fields } = res.data;
-      setOne(fields.one);
-      setTwo(fields.two);
-      setThree(fields.three);
-      setFour(fields.four);
-      setFive(fields.five);
-      setSix(fields.six);
-      setSeven(fields.seven);
-      setEight(fields.eight);
-      setNine(fields.nine);
-      setTen(fields.ten);
-      setEleven(fields.eleven);
-      setTwelve(fields.twelve);
-      setThirteen(fields.thirteen);
-      setFourteen(fields.fourteen);
-      setFifteen(fields.fifteen);
-      setSixteen(fields.sixteen);
-      setSeventeen(fields.seventeen);
-      setEighteen(fields.eighteen)
+      const { fields } = res?.data;
+      setOne(fields?.one);
+      setTwo(fields?.two);
+      setThree(fields?.three);
+      setFour(fields?.four);
+      setFive(fields?.five);
+      setSix(fields?.six);
+      setSeven(fields?.seven);
+      setEight(fields?.eight);
+      setNine(fields?.nine);
+      setTen(fields?.ten);
+      setEleven(fields?.eleven);
+      setTwelve(fields?.twelve);
+      setThirteen(fields?.thirteen);
+      setFourteen(fields?.fourteen);
+      setFifteen(fields?.fifteen);
+      setSixteen(fields?.sixteen);
+      setSeventeen(fields?.seventeen);
+      setEighteen(fields?.eighteen)
 
 
     }
